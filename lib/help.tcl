9db78da6ac0879f03d9ff09443b37dccdd6bc330
Steve Bennett <steveb@workware.net.au>
2012-02-13 15:28:07 +1000

    Add -d? to show debugging types

--- lib/help.tcl
+++ lib/help.tcl
@@ -16,8 +16,10 @@ proc show-help {argv} {
    -h|--help             This help, or help for the specified rule
    -C|--directory=<dir>  Run as if from directory <dir>
    -v|--verbose          Force V=1 mode when building to show commands executed
-   -d|--debug            Enable debugging output
    -n|--dry-run          Show commands which would have been run
+   -d...                 Enable various debugging "types"
+   -d?                   Show all individual debugging types
+   --debug               Alternative to "-dg"
 }
 	puts [show-version]
 	exit 0
