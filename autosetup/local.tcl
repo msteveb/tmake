04531659d104dd95343c3a1b61f23ac68017e60d
Steve Bennett <steveb@workware.net.au>
2013-09-04 11:49:52 +1000

    Add -Qunused-arguments if supported by the compiler

--- autosetup/local.tcl
+++ autosetup/local.tcl
@@ -2,7 +2,7 @@ use cc
 
 # If the compiler supports -Qunused-arguments, enable it
 # This is to prevent clang from producing annoying messages
-if {[cctest -cflags {-Qunused-arguments}]} {
+if {[cctest -nooutput 1 -cflags {-Qunused-arguments}]} {
 	define-append CC -Qunused-arguments
 }
 
