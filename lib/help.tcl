fc86065c3ee552bbce8d463fe1c5a63e3a458492
Steve Bennett <steveb@workware.net.au>
2014-11-27 09:02:28 +1000

    Start to improve the documentation
    Make tmake --ref at least work
    Add tmake --rules
    Use a pager by default, but turn off with --nopager

--- lib/help.tcl
+++ lib/help.tcl
@@ -25,6 +25,7 @@ proc show-help {argv} {
    -p|--print            Output all known rules
    --genie               Generate an initial build.spec from sources in the current dir
    --ref                 Show command reference
+   --rules               Show rules from the rulebase
    -d...                 Enable various debugging "types"
    -d?                   Show all individual debugging types
    --debug               Alternative to "-dg"
@@ -43,36 +44,72 @@ proc show-help {argv} {
 	exit 0
 }
 
-# Outputs the references manual in one of several formats
-proc show-reference {{type text}} {
+# If not already paged and stdout is a tty, pipe the output through the pager
+# This is done by reinvoking autosetup with --nopager added
+proc use_pager {} {
+    if {$::tmake(usepager) && [getenv PAGER ""] ne "" && [isatty? stdin] && [isatty? stdout]} {
+        if {[catch {
+            exec [info nameofexecutable] $::argv0 --nopager {*}$::argv |& {*}[getenv PAGER] >@stdout <@stdin 2>@stderr
+        } msg opts] == 1} {
+            if {[dict get $opts -errorcode] eq "NONE"} {
+                # an internal/exec error
+                puts stderr $msg
+                exit 1
+            }
+        }
+        exit 0
+    }
+}
 
-    #use_pager
-
+proc help-select-formatter {type} {
     switch -glob -- $type {
         wiki {use wiki-formatting}
         ascii* {use asciidoc-formatting}
         md - markdown {use markdown-formatting}
         default {use text-formatting}
     }
+}
+
+# Outputs the reference manual in one of several formats
+proc show-reference {{type text}} {
+
+    use_pager
+    help-select-formatter $type
 
     title "[show-version] -- Command Reference"
 
     section {Introduction}
 
     p {
-        See http://xxx/ for the online documentation for 'tmake'
+        See https://dev.workware.net.au/git/?p=tmake.git;a=summary for the online documentation for 'tmake'
     }
 
     p {
-        'autosetup' provides a number of built-in commands which
-        are documented below. These may be used from 'auto.def' to test
-        for features, define variables, create files from templates and
-        other similar actions.
+        'tmake' is a concept build system. It is used as a test bench
+        for various ideas for a simple but powerful build system, while
+        also being useful for real-world projects.
+
+        'tmake' is not yet ready for public consumption. It lacks a number
+        of key features and much documentation.
     }
 
     show-command-reference
 
     exit 0
+}
+
+proc show-rules {{type text}} {
+
+    use_pager
+    help-select-formatter $type
+
+    set rulebase [get-rulebase]
+    if {[llength $rulebase] == 1} {
+        lassign $rulebase file
+        show-command-reference-blob [file rootname [file tail $file]] [readfile $file]
+    } else {
+        show-command-reference-blob {*}$rulebase
+    }
 }
 
 proc output-help-block {type lines} {
@@ -101,18 +138,18 @@ proc show-command-reference {} {
 
     set rulebase [get-rulebase]
     if {[llength $rulebase] == 1} {
+        # External rulebase
         lappend files [lindex $rulebase 0]
     }
 
     section "Core Commands"
-    set type p
-    set lines {}
-    set cmd {}
 
     foreach file $files {
         show-command-reference-blob [file rootname [file tail $file]] [readfile $file]
     }
+
     if {[llength $rulebase] > 1} {
+        # Default/internal rulebase
         show-command-reference-blob {*}$rulebase
     }
     exit 0
---
+++ lib/text-formatting.tcl
@@ -0,0 +1,88 @@
+# Copyright (c) 2010 WorkWare Systems http://www.workware.net.au/
+# All rights reserved
+
+# Module which provides text formatting
+
+use formatting
+
+proc wordwrap {text length {firstprefix ""} {nextprefix ""}} {
+    set len 0
+    set space $firstprefix
+    foreach word [split $text] {
+        set word [string trim $word]
+        if {$word == ""} {
+            continue
+        }
+        if {$len && [string length $space$word] + $len >= $length} {
+            puts ""
+            set len 0
+            set space $nextprefix
+        }
+        incr len [string length $space$word]
+
+        # Use man-page conventions for highlighting 'quoted' and *quoted*
+        # single words.
+        # Use x^Hx for *bold* and _^Hx for 'underline'.
+        #
+        # less and more will both understand this.
+        # Pipe through 'col -b' to remove them.
+        if {[regexp {^'(.*)'([^a-zA-Z0-9_]*)$} $word -> bareword dot]} {
+            regsub -all . $bareword "_\b&" word
+            append word $dot
+        } elseif {[regexp {^[*](.*)[*]([^a-zA-Z0-9_]*)$} $word -> bareword dot]} {
+            regsub -all . $bareword "&\b&" word
+            append word $dot
+        }
+        puts -nonewline $space$word
+        set space " "
+    }
+    if {$len} {
+        puts ""
+    }
+}
+proc title {text} {
+    underline [string trim $text] =
+    nl
+}
+proc p {text} {
+    wordwrap $text 80
+    nl
+}
+proc codelines {lines} {
+    foreach line $lines {
+        puts "    $line"
+    }
+    nl
+}
+proc nl {} {
+    puts ""
+}
+proc underline {text char} {
+    regexp "^(\[ \t\]*)(.*)" $text -> indent words
+    puts $text
+    puts $indent[string repeat $char [string length $words]]
+}
+proc section {text} {
+    underline "[string trim $text]" -
+    nl
+}
+proc subsection {text} {
+    underline "$text" ~
+    nl
+}
+proc bullet {text} {
+    wordwrap $text 76 "  * " "    "
+}
+proc indent {text} {
+    wordwrap $text 76 "    " "    "
+}
+proc defn {first args} {
+    if {$first ne ""} {
+        underline "    $first" ~
+    }
+    foreach p $args {
+        if {$p ne ""} {
+            indent $p
+        }
+    }
+}
